efffc7a7a8b83c2a49419b6bf1ccbae5e9ac58cd
Mateus Oliveira <116849388+mateusgon97@users.noreply.github.com>
2024-11-27 22:58:39 -0300

    atualização de branch

--- UC06/src/main/java/com/lancamento/vendas/service/ProdutoService.java
+++ UC06/src/main/java/com/lancamento/vendas/service/ProdutoService.java
@@ -1,6 +1,7 @@
 package com.lancamento.vendas.service;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,57 +20,75 @@ public class ProdutoService {
 
     @Autowired
     private VendaRepository vendaRepository;
-
-    // Verificar se o produto existe
-    public Produto verificarProdutoExistente(Long produtoId) throws Exception {
-        Optional<Produto> produtoOpt = produtoRepository.findById(produtoId);
-        if (!produtoOpt.isPresent()) {
-            throw new Exception("Produto não encontrado! ID: " + produtoId);
-        }
-        return produtoOpt.get();
+    
+    
+    
+    public List<Produto> listarTodos() {
+        return produtoRepository.findAll();
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
-    // Verificar se o estoque é suficiente
-    public void verificarEstoqueSuficiente(Produto produto, int quantidadeVendida) throws Exception {
-        if (produto.getQuantidade() <= quantidadeVendida) {
-            throw new Exception("Estoque insuficiente para o produto: " + produto.getNome());
-        }
-    }
+//    // Verificar se o produto existe
+//    public Produto verificarProdutoExistente(Long produtoId) throws Exception {
+//        Optional<Produto> produtoOpt = produtoRepository.findById(produtoId);
+//        if (!produtoOpt.isPresent()) {
+//            throw new Exception("Produto não encontrado! ID: " + produtoId);
+//        }
+//        return produtoOpt.get();
+//    }
+//
+//    // Verificar se o estoque é suficiente
+//    public void verificarEstoqueSuficiente(Produto produto, int quantidadeVendida) throws Exception {
+//        if (produto.getQuantidade() <= quantidadeVendida) {
+//            throw new Exception("Estoque insuficiente para o produto: " + produto.getNome());
+//        }
+//    }
+//
+//    // Calcular o valor total da venda
+//    public double calcularValorVenda(Produto produto, int quantidadeVendida) {
+//        return produto.getPrecoVenda() * quantidadeVendida;
+//    }
+//
+//    // Criar a venda
+//    public Venda criarVenda(Long produtoId, int quantidadeVendida, double valorTotalVenda) {
+//        Venda venda = new Venda();
+//        venda.setProdutoId(produtoId);
+//        venda.setQuantidadeVendida(quantidadeVendida);
+//        venda.setValorTotalVenda(valorTotalVenda);
+//        venda.setDataHoraVenda(new Date());
+//        return venda;
+//    }
+//
+//    // Atualizar o estoque do produto
+//    public void atualizarEstoque(Produto produto, int quantidadeVendida) {
+//        produto.setQuantidade(produto.getQuantidade() - quantidadeVendida);
+//        produtoRepository.save(produto);
+//    }
+//
+//    public Venda realizarVenda(Venda venda) throws Exception {
+//        Produto produto = verificarProdutoExistente(venda.getProdutoId());
+//        verificarEstoqueSuficiente(produto, venda.getQuantidadeVendida());
+//
+//        double valorTotalVenda = calcularValorVenda(produto, venda.getQuantidadeVendida());
+//
+//        venda.setValorTotalVenda(valorTotalVenda);
+//        venda.setDataHoraVenda(new Date());
+//
+//        vendaRepository.save(venda);
+//
+//        atualizarEstoque(produto, venda.getQuantidadeVendida());
+//
+//        return venda;
+//    }
+    
 
-    // Calcular o valor total da venda
-    public double calcularValorVenda(Produto produto, int quantidadeVendida) {
-        return produto.getPrecoVenda() * quantidadeVendida;
-    }
-
-    // Criar a venda
-    public Venda criarVenda(Long produtoId, int quantidadeVendida, double valorTotalVenda) {
-        Venda venda = new Venda();
-        venda.setProdutoId(produtoId);
-        venda.setQuantidadeVendida(quantidadeVendida);
-        venda.setValorTotalVenda(valorTotalVenda);
-        venda.setDataHoraVenda(new Date());
-        return venda;
-    }
-
-    // Atualizar o estoque do produto
-    public void atualizarEstoque(Produto produto, int quantidadeVendida) {
-        produto.setQuantidade(produto.getQuantidade() - quantidadeVendida);
-        produtoRepository.save(produto);
-    }
-
-    public Venda realizarVenda(Venda venda) throws Exception {
-        Produto produto = verificarProdutoExistente(venda.getProdutoId());
-        verificarEstoqueSuficiente(produto, venda.getQuantidadeVendida());
-
-        double valorTotalVenda = calcularValorVenda(produto, venda.getQuantidadeVendida());
-
-        venda.setValorTotalVenda(valorTotalVenda);
-        venda.setDataHoraVenda(new Date());
-
-        vendaRepository.save(venda);
-
-        atualizarEstoque(produto, venda.getQuantidadeVendida());
-
-        return venda;
-    }
 }
